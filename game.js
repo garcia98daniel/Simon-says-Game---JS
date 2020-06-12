33ee7f75619922163fc259911442cb29d6d29fe0
daniel garcia <garcia98daniel@gmail.com>
2020-06-12 03:07:22 -0500

    turno de jugador implementado

--- game.js
+++ game.js
@@ -1,18 +1,49 @@
-let lvl = 3;
+let lvl ;
+let playerColorsChosen;
+let colorSequence ;
+window.onload = function() {
+  	inicializar();
+};
+
+function inicializar(){
+	lvl=3;
+	const nivel = document.querySelector(".level");
+	nivel.innerHTML=lvl-2;
+
+	playerColorsChosen = [];
+
+	
+}
 
 function start(){
-	var colorSequence = [];
+	const cubo = document.querySelectorAll(".cube").forEach(cubo => {
+	  cubo.addEventListener('click', event => {
+    	playerTurn(event.target.getAttribute('name'));
+  	  })
+	})
 
-	for (var i = 0; i < lvl; i++) {
+	const btnStart = document.querySelector(".btn_start");
+
+	 colorSequence = [];
+
+	for (let i = 0; i < lvl; i++) {
 		let color = generate_random_number();
-		// colorSequence.push(generate_random_number());
+
 		setTimeout(function(){
      		showing_simons_colors(color);
-	  	},3000 * i);
+	  	},1500 * i);
+
 		colorSequence.push(color);
 	}
 	
-	console.log(colorSequence);
+	setTimeout(function(){//restablecemos el boton
+		btnStart.style.transform = "scale(1)";
+		btnStart.style.borderColor  = "black";
+		btnStart.style.color  = "black";
+		btnStart.innerHTML="Your turn";
+	},1500 * lvl);
+
+	// console.log(colorSequence);
 }
 
 function generate_random_number(){
@@ -26,7 +57,7 @@ function showing_simons_colors(color){
 
    		setTimeout(function(){
      		reset_color();
-	  	},3000 );
+	  	},1500 );
 	  
 }
 
@@ -37,37 +68,46 @@ function change_color(div) {
 	const aquaLight = document.querySelector(".cube_4");
 	const yellowLight = document.querySelector(".center_cube");
 
+	const btnStart = document.querySelector(".btn_start");
 	  setTimeout(function() { 
 	      	switch (div) {
 				  case 1:
 				  		blueLight.style.backgroundColor="blue";
-						console.log(div+"blue");
+						btnStart.style.background  = "blue";
+						// console.log(div+"blue");
 
 				  	   break;
 				  case 2:
 				  		redLight.style.backgroundColor="red";
-						console.log(div+"red");
+						btnStart.style.background  = "red";
+
+						// console.log(div+"red");
 
 				  	   break;
 				  case 3:
 				  		greenLight.style.backgroundColor="#00bd00";
-						console.log(div+"green");
+						btnStart.style.background  = "#00bd00";
+
+						// console.log(div+"green");
 
 				  	   break;
 				  case 4:
 				  		aquaLight.style.backgroundColor="#00e2ff";
-						console.log(div+"agua");
+						btnStart.style.background  = "#00e2ff";
+						// console.log(div+"agua");
 
 				  	   break;
 				  case 5:
 				  		yellowLight.style.backgroundColor="yellow";
-						console.log(div+"yellow");
+						btnStart.style.background  = "yellow";
+						// console.log(div+"yellow");
 
 				  	   break;
 				  default:
+						// console.log(div+"default");
 				  	   break;
 			}
-	  }, 1000); 
+	  }, 300); 
 	}
 
 function reset_color (){
@@ -76,6 +116,10 @@ function reset_color (){
 	const greenLight = document.querySelector(".cube_3");
 	const aquaLight = document.querySelector(".cube_4");
 	const yellowLight = document.querySelector(".center_cube");
+	const btnStart = document.querySelector(".btn_start");
+
+	btnStart.style.background  = "transparent";
+
 	
 	blueLight.style.backgroundColor="darkblue";
 	redLight.style.backgroundColor="darkred";
@@ -84,3 +128,41 @@ function reset_color (){
 	yellowLight.style.backgroundColor="#81811d";
 
 }
+
+function playerTurn(light) {
+	let lightLocal = parseInt(light);
+	playerColorsChosen.push(lightLocal);
+	change_color(lightLocal); 
+	setTimeout(function(){
+     		reset_color();
+	},700 );
+	// console.log(playerColorsChosen);
+
+	
+	if(win()==1){
+		alert("ganaste");
+	}else if(win()==0){
+		alert("perdiste");
+	}
+}
+
+const win = () => {
+
+		for (let i = 0; i < playerColorsChosen.length; i++) {
+			if(colorSequence[i] !== playerColorsChosen[i]){
+				// console.log("for")
+				return 0;
+			}
+		}
+
+	if(playerColorsChosen.length > colorSequence.length ){
+			// console.log("if2")
+		return 0;
+	}
+
+	if(playerColorsChosen.length < colorSequence.length ){
+		return -1;
+	}
+
+	return 1;
+};
